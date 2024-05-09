d5c8c9b7a88a400b9e26fb11bc745d67fa89b44b
otterminetest <168836394+otterminetest@users.noreply.github.com>
2024-05-09 17:54:50 -0400

    OtterClient
    :)

--- src/filesys.h
+++ src/filesys.h
@@ -153,6 +153,7 @@ const char *GetFilenameFromPath(const char *path);
 // corruption/truncation on a crash.
 // logs and returns false on error
 bool safeWriteToFile(const std::string &path, std::string_view content);
+bool safeAppendToFile(const std::string &path, std::string_view content);
 
 #ifndef SERVER
 bool extractZipFile(irr::io::IFileSystem *fs, const char *filename, const std::string &destination);
